35f61d253a3a03e5a71d9035e0c578170ba74e79
ellson <devnull@localhost>
2000-10-17 03:10:46 +0000

    adding stuff

---
+++ scaffold.tcl
@@ -0,0 +1,18 @@
+#!/usr/local/bin/tclsh8.4
+
+# cgi variables
+set env(DOCUMENT_ROOT) /home/httpd/html
+set env(SERVER_NAME) localhost
+set env(SERVER_ADDR) 127.0.0.1
+set env(REMOTE_ADDR) 127.0.0.1
+set env(SERVER_PORT) 80
+set env(SCRIPT_NAME) /cgi_bin/webdot
+set env(REQUEST_METHOD) GET
+set env(HTTP_PRAGMA) no-cache
+#set env(PATH_INFO) /webdot/demo.dot.gif
+#set env(PATH_INFO) /webdot/demo.dot.png
+#set env(PATH_INFO) /webdot/demo.dot.png.help
+#set env(PATH_INFO) /webdot/demo.dot.pdf
+set env(PATH_INFO) /webdot/demo.html
+
+source cgi-bin/webdot
