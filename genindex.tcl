63572eab06eda6a29b6ed50b821ac40da51355f2
ellson <devnull@localhost>
2000-11-14 04:57:31 +0000

    work on demo page

--- genindex.tcl
+++ genindex.tcl
@@ -14,14 +14,15 @@ puts $f "<html>
 <h1><a href=$w>WebDot</a> Demo Graphs</h1>
 <table border=1>"
 
+set e dot
 set i 0
 foreach d [lsort [glob *.dot]] {
 	if {$i%4} {puts $f "<td><td>"}
 	puts $f "<td><a href=$w/$p/$d.src>$d</a></td>
-<td><a href=$w/$p/$d.png>png</a></td>
-<td><a href=$w/$p/$d.tcl>tcl</a></td>
-<td><a href=$w/$p/$d.ps>ps</a></td>
-<td><a href=$w/$p/$d.pdf>pdf</a></td>"
+<td><a href=$w/$p/$d.$e.png>png</a></td>
+<td><a href=$w/$p/$d.$e.tcl>tcl</a></td>
+<td><a href=$w/$p/$d.$e.ps>ps</a></td>
+<td><a href=$w/$p/$d.$e.pdf>pdf</a></td>"
 	if {!([incr i]%4)} {puts $f "</tr><tr>"}
 }
 
@@ -30,13 +31,13 @@ puts $f "</tr>
 <p>
 For viewing <b>pdf</b> files you will need the pdf plugin:
 <a href=http://www.adobe.com/prodindex/acrobat/readstep.html>
-<IMG SRC=getacro.png WIDTH=89 HEIGHT=30 ALIGN=CENTER
-BORDER=0 ALT=\"get acrobat reader\"></a>
+<img src=getacro.png width=89 height=30 align=center
+border=0 alt=\"get acrobat reader\"></a>
 <p> 
 For viewing <b>tcl</b> files you will need the tcl plugin:
 <a href=http://www.scriptics.com/plugin/>
-<IMG SRC=tclp.png WIDTH=42 HEIGHT=64 ALIGN=CENTER
-BORDER=0 ALT=\"get tcl plugin\"></a>
+<img src=tclp.png width=42 height=64 align=center
+border=0 alt=\"get tcl plugin\"></a>
 
 <p>
 The graph renderings are produced on-demand by the 
