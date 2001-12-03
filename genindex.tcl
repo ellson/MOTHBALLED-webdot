ddf89b4ae75106adf5472aff9dd33d52b0ddab10
ellson <devnull@localhost>
2001-12-03 22:50:33 +0000

    modified and generated files

--- genindex.tcl
+++ genindex.tcl
@@ -1,13 +1,19 @@
 #!/usr/bin/tclsh
 
-set f [open html/webdot/demo.html w]
-set h /webdot/
+cd html
+set oldpwd [pwd]
+
+set h /webdot
 set w /cgi-bin/webdot
+
+set g $h/graphs
+set f [open .$h/demo.html w]
 
 set layout_engines {dot neato twopi}
 set nl [llength $layout_engines]
 
-set formats {{png help} {gif help} {tcl help} ps pdf svg}
+#set formats {{png help} {gif help} {tcl help} ps pdf svg}
+set formats {{png help} pdf svg}
 set nf [llength $formats]
 
 set directions {Directed Undirected}
@@ -18,17 +24,16 @@ set ncol 2
 puts $f "<html>
 <head>
 <title>WebDot Demo Graphs</title>
+<link rel=\"shortcut icon\" href=\"$h/icon.png\">
 </head>
 <body bgcolor=#ffffff>
-<h1><a href=$h>WebDot</a> Demo Graphs</h1>
+<h1><a href=$h/>WebDot</a> Demo Graphs</h1>
 <table border=1><tr>"
-
-set oldpwd [pwd]
 
 set k 0
 foreach dir {Directed Undirected} {
-  set p /webdot/graphs/[string tolower $dir]
-  cd $oldpwd/html/webdot/graphs/[string tolower $dir]
+  set p $g/[string tolower $dir]
+  cd $oldpwd$g/[string tolower $dir]
 
   for {set i 1} {$i <= $ncol} {incr i} {
     puts $f "<th>$dir</th><th colspan=$nf>DOT</th>"
@@ -75,25 +80,46 @@ puts $f "</tr>
 <p>
 <table>
 <tr>
-<td>For viewing <b>pdf</b> files you will need the pdf plugin:</td>
+<td>For viewing <b>svg</b> files you will need a pdf plugin such as:</td>
+<td><a href=http://www.adobe.com/svg/viewer/install/old.html>
+   <img src=svgdownload.gif
+	width=88
+	height=31
+	align=center
+	border=0
+	alt=\"get acrobat svg plugin\">
+</a></td>
+<td>For viewing <b>pdf</b> files you will need a pdf plugin such as:</td>
 <td><a href=http://www.adobe.com/prodindex/acrobat/readstep.html>
-<img src=getacro.png width=89 height=30 align=center
-border=0 alt=\"get acrobat reader\"></a></td>
-<td>and for viewing <b>tcl</b> files you will need the tcl plugin:</td>
+   <img src=getacro.png
+	width=89
+	height=30
+	align=center
+	border=0
+	alt=\"get acrobat reader\">
+</a></td>
+<!--
+<td>For for viewing <b>tcl</b> files you will need the tcl plugin:</td>
 <td><a href=http://www.demailly.com/tcl/plugin/download.html>
-<img src=tclp.png width=42 height=64 align=center
-border=0 alt=\"get tcl plugin\"></a></td>
+   <img src=tclp.png
+	width=42
+	height=64
+	align=center
+	border=0
+	alt=\"get tcl plugin\">
+</a></td>
+-->
 </tr>
 </table>
 
 <p>
 The graph renderings are produced on-demand by the 
-<a href=$h>WebDot Graph Server</a>.
+<a href=$h/>WebDot Graph Server</a>.
 Please send problem reports first to the person that installed
 this server locally.  WebDot was written by:
 <a href=mailto:ellson@lucent.com>John Ellson</a>.
 <p>
-Thanks for trying <a href=$h>WebDot</a>.
+Thanks for trying <a href=$h/>WebDot</a>.
 </body>
 </html>"
 
--- webdot.spec
+++ webdot.spec
@@ -1,5 +1,5 @@
 Name:           webdot
-Version:        1.7.11
+Version:        1.7.12
 Release:        1_RH7
 Group:          Applications/Graphics
 Copyright:      BSD-style
