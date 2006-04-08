7b7be4ba9f683f54283aa1777dc1f4fc440c3330
ellson <devnull@localhost>
2006-04-08 03:04:39 +0000

    add another <meta ...>

--- linktut.tcl
+++ linktut.tcl
@@ -27,6 +27,7 @@ set OTHER {
 set header {<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
+<meta name="robots" content="noindex,nofollow">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="shortcut icon" href="$HTMLBASE/icon.png">
 <title>WebDot Tutorial: $TITLE</title>
