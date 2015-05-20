67a7c7983f07c10860a65ac02dbb70194ff1a35a
John Ellson <ellson@research.att.com>
2015-05-20 15:50:03 -0400

    always use date of last commit in version, even for stable

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,8 @@ if test $? -eq 0; then
     WEBDOT_VERSION_DATE=$( date -u +%Y%m%d.%H%M -d @$WEBDOT_VERSION_DATE )
     echo "Version date is based on time of last commit: $WEBDOT_VERSION_DATE"
 else
-    WEBDOT_VERSION_DATE=$( date -u +%Y%m%d.%H%M )
-    echo "Warning: we do not appear to be running in a git clone."
-    echo "Version date is based on time now: $WEBDOT_VERSION_DATE"
+    WEBDOT_VERSION_DATE="0"
+    echo "Warning: we do not appear to be running in a git clone." >&2
 fi
 
 # initialize version for a "stable" build
@@ -21,7 +20,7 @@ dnl                       timestamp => tar-file snapshot or release
 m4_define(webdot_version_major, 2)
 m4_define(webdot_version_minor, 39)
 dnl NB: the next line gets changed to a date/time string for development releases
-m4_define(webdot_version_micro, 0)
+m4_define(webdot_version_micro, $WEBDOT_VERSION_DATE)
 m4_define(webdot_version_date, $WEBDOT_VERSION_DATE)
 m4_define(webdot_collection, test)
 m4_define(webdot_version_commit, unknown)
