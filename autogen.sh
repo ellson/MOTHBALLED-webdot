360b5562574967c5b1335eee34b499c80e384cfd
John Ellson <ellson@research.att.com>
2015-05-20 12:49:53 -0400

    align versioning mechanism with graphviz

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+WEBDOT_VERSION_DATE=$( git log -n 1 --format=%ct )
+if test $? -eq 0; then
+    WEBDOT_VERSION_DATE=$( date -u +%Y%m%d.%H%M -d @$WEBDOT_VERSION_DATE )
+    echo "Version date is based on time of last commit: $WEBDOT_VERSION_DATE"
+else
+    WEBDOT_VERSION_DATE=$( date -u +%Y%m%d.%H%M )
+    echo "Warning: we do not appear to be running in a git clone."
+    echo "Version date is based on time now: $WEBDOT_VERSION_DATE"
+fi
+
+# initialize version for a "stable" build
+cat >./version.m4 <<EOF
+dnl webdot package version number, (as distinct from shared library version)
+dnl For the minor number: odd => unstable series
+dnl                       even => stable series
+dnl For the micro number: 0 => in-progress development
+dnl                       timestamp => tar-file snapshot or release
+m4_define(webdot_version_major, 2)
+m4_define(webdot_version_minor, 39)
+dnl NB: the next line gets changed to a date/time string for development releases
+m4_define(webdot_version_micro, 0)
+m4_define(webdot_version_date, $WEBDOT_VERSION_DATE)
+m4_define(webdot_collection, test)
+m4_define(webdot_version_commit, unknown)
+EOF
+
+
 # default tools
 #AUTOMAKE=automake
 #ACLOCAL=aclocal
